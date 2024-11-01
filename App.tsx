892b819a616efa18eec40788197c8c18e882467d
truongthanhdat <truongdatzzz612@gmail.com>
2024-11-01 21:50:35 +0700

    init project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState<number>(0);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Count = {count}</Text>
+      <Button title='Click me' onPress={() => setCount(count + 1)}></Button>
       <StatusBar style="auto" />
     </View>
   );
