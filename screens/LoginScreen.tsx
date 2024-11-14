de65c967968a5b45daa90d8a2901fda1a3aeb62e
truongthanhdat <truongdatzzz612@gmail.com>
2024-11-14 23:45:00 +0700

    updateLogin

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Alert, TouchableOpacity, StatusBar, Image } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Entypo from '@expo/vector-icons/Entypo';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
+import iconGoogle from '../assets/img/Google_Icons.jpg';
+import iconFacebook from '../assets/img/Facebook_icon.jpg';
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('1');
@@ -25,14 +27,24 @@ const LoginScreen = () => {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Đăng Nhập</Text>
+            <StatusBar
+                animated={true}
+                backgroundColor="#b0272f"
+                barStyle="dark-content"     // Options: 'default', 'light-content', 'dark-content'
+            />
+
+            <View>
+                <Text style={styles.title}>Chào mừng bạn đến với</Text>
+                <Text style={styles.title1}>Cà Phê 2003's</Text>
+            </View>
+
             <TextInput
                 style={styles.input}
                 placeholder="Email"
                 keyboardType="email-address"
                 value={email}
                 onChangeText={setEmail}
-                placeholderTextColor={"#FFD15C"}
+                placeholderTextColor={"gray"}
             />
 
             <View style={styles.passwordContainer}>
@@ -42,63 +54,90 @@ const LoginScreen = () => {
                     secureTextEntry={showPassword}
                     value={password}
                     onChangeText={setPassword}
-                    placeholderTextColor={"#FFD15C"}
+                    placeholderTextColor={"gray"}
                 />
 
                 <TouchableOpacity onPress={() => setShowPassword(!showPassword)} style={styles.eyeIcon}>
                     <Entypo
-                        name={!showPassword ? "eye"  : "eye-with-line"}  // Hiển thị biểu tượng "eye" nếu showPassword = true, "eye-with-line" nếu false
+                        name={!showPassword ? "eye-with-line" : "eye"} // Hiển thị biểu tượng "eye-with-line" nếu showPassword = true, "eye" nếu false : }  // Hiển thị biểu tượng "eye" nếu showPassword = true, "eye-with-line" nếu false
                         size={20}
-                        color="#fff"
+                        color="black"
                     />
                 </TouchableOpacity>
             </View>
 
+            <View style={styles.forgotPasswordContainer}>
+                <TouchableOpacity style={styles.forgotPassword} onPress={() => alert("Quen mat khau")}>
+                    <Text style={styles.forgotPasswordTitle}>Quên Mật Khẩu?</Text>
+                </TouchableOpacity>
+            </View>
+
+
+            <View style={styles.buttonSubmit}>
+                <Button title="Đăng Nhập" onPress={handleLogin} />
+            </View>
+
+
+            <View style={styles.line}>
+                <Text style={styles.lineText}></Text>
+                <Text style={styles.lineTitle}>hoặc</Text>
+                <Text style={styles.lineText}></Text>
+            </View>
+
 
             <View style={styles.externalIcon}>
-                <View style={styles.icon}>
-                    <AntDesign name="google" size={30} color="white" />
-                </View>
-                <View style={styles.icon}>
-                    <AntDesign name="facebook-square" size={30} color="blue" />
-                </View>
-            </View>
-            <View style={{ alignItems: "center" }}>
-                <TouchableOpacity style={styles.register} onPress={handleRegister}>
-                    <Text style={styles.registerTitle}>Bạn chưa có tài khoản? Đăng ký ngay!</Text>
+                <TouchableOpacity style={styles.icon} onPress={() => alert("Google")}>
+                    <Image source={iconGoogle} style={styles.iconImage} />
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.forgotPassword} onPress={() => alert("Quen mat khau")}>
-                    <Text style={styles.forgotPasswordTitle}>Quên mật khẩu</Text>
+                <TouchableOpacity style={styles.icon} onPress={() => alert("Facebook")}>
+                    <Image source={iconFacebook} style={styles.iconImage} />
                 </TouchableOpacity>
             </View>
 
-            <Button title="Đăng Nhập" onPress={handleLogin} />
+            <View style={styles.registerContainer}>
+                <View style={styles.register} >
+                    <Text style={styles.registerTitle}>Khách Hàng Mới?</Text>
+                    <TouchableOpacity onPress={handleRegister}>
+                        <Text style={styles.registerTitle2}>
+                            Tạo Một Tài Khoản!
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
         </View>
+
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         justifyContent: 'center',
-        padding: 16,
-        backgroundColor: '#662C1B',
+        padding: 20,
+        backgroundColor: '#fff',
         height: "100%",
     },
     title: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: "gray",
+    },
+    title1: {
         fontSize: 24,
         fontWeight: 'bold',
         textAlign: 'center',
         marginBottom: 24,
-        color: "#FFD15C",
+        color: "#b03b47",
     },
     input: {
         height: 40,
-        borderColor: '#FFD15C',
+        borderColor: 'gray',
         borderWidth: 1,
         borderRadius: 8,
         paddingHorizontal: 10,
         marginBottom: 12,
-        color: "#FFD15C",
+        color: "black",
     },
     passwordContainer: {
         position: 'relative',
@@ -108,32 +147,76 @@ const styles = StyleSheet.create({
         right: 10,
         top: 10,
     },
+    buttonSubmit: {
+        marginBottom: 10,
+    },
     icon: {
-        margin: 10
+        margin: 10,
+    },
+    iconImage: {
+        width: 40,
+        height: 40,
+        resizeMode: 'contain',
+        borderWidth: 1,
+        borderColor: "gray",
+        borderRadius: 50,
+        marginHorizontal: 20,
     },
     externalIcon: {
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "center",
     },
+    registerContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+    },
     register: {
+        top: 190,
+        flexDirection: "row",
+        alignItems: "center",
         marginVertical: 5,
     },
     registerTitle: {
-        fontSize: 15,
-        color: "blue",
-        textDecorationLine: "underline",
+        fontSize: 17,
+        color: "#88776e",
         fontWeight: "400",
+    },
+    registerTitle2: {
+        fontSize: 17,
+        color: "#b03b47",
+        fontWeight: "bold",
+        marginLeft: 10,
+    },
+    forgotPasswordContainer: {
+        alignItems: "flex-end",
+        marginBottom: 20,
     },
     forgotPassword: {
         marginVertical: 5,
     },
     forgotPasswordTitle: {
-        fontSize: 15,
-        color: "blue",
-        textDecorationLine: "underline",
-        fontWeight: "400",
+        fontSize: 14,
+        color: "#b03b47",
+        fontWeight: "bold",
     },
+    line: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginVertical: 20,
+    },
+    lineText: {
+        flex: 1,
+        height: 2,
+        backgroundColor: "black",
+    },
+    lineTitle: {
+        paddingHorizontal: 20,
+        color: "black",
+        fontSize: 15,
+        fontWeight: "bold",
+    }
 });
 
 export default LoginScreen;
